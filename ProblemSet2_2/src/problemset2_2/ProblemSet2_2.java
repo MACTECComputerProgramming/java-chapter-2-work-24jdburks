51d44e2e86dfe7bf5173c5d130f069870c4c9fbf
rtracy14 <89794542+24jdburks@users.noreply.github.com>
2021-09-16 11:00:15 -0500

    first push

---
+++ ProblemSet2_2/src/problemset2_2/ProblemSet2_2.java
@@ -0,0 +1,76 @@
+package problemset2_2;
+
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+       
+        
+        
+        
+        System.out.println("x");
+                System.out.println("x");               
+                System.out.println("x");              
+                System.out.println("x");              
+                System.out.println("x");              
+                System.out.println("x");              
+                System.out.println("x");              
+                System.out.println("x");              
+                System.out.println("x");              
+                System.out.println("xxxxxxxxxxxx");    
+                
+                
+               
+                
+                
+            
+                System.out.println("xxxxxxxxxxxx");
+                System.out.println("x          x");
+                System.out.println("x          x");
+                System.out.println("x          x");
+                System.out.println("x          x");
+                System.out.println("x          x");
+                System.out.println("x          x");
+                System.out.println("x          x");
+                System.out.println("x          x");
+                System.out.println("x          x");
+                System.out.println("xxxxxxxxxxxx");
+                
+                
+                
+                
+                System.out.println("x");
+                System.out.println("x");               
+                System.out.println("x");              
+                System.out.println("x");              
+                System.out.println("x");              
+                System.out.println("x");              
+                System.out.println("x");              
+                System.out.println("x");              
+                System.out.println("x");              
+                System.out.println("xxxxxxxxxxxx");  
+                
+                
+                
+                
+
+
+
+
+
+
+
+
+
+
+
+//Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+
+        
+        
+        
+        
+        
+        
+        
+    } 
+}
